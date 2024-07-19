7e7114eda85cad22834c74a9e481d6ef946e2c72
dejan <dejan.pelemis@cyberlab.rs>
2024-07-19 15:43:34 +0200

    feat: Added modules for bucket, kms, dynamo db

--- dynamodb/main.tf
+++ dynamodb/main.tf
@@ -4,8 +4,9 @@ module "meta" {
 }
 
 module "dynamodb-table" {
-  source  = "github.com/terraform-aws-modules/terraform-aws-dynamodb-table?ref=696ceabbfdd49f8246e3d401c035729d60ea6fab"
-  
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = "3.3.0"
+
   name = var.table_name != "" ? var.table_name : module.meta.name
 
   autoscaling_enabled = var.autoscaling_enabled
--- s3/main.tf
+++ s3/main.tf
@@ -3,9 +3,11 @@ module "meta" {
   meta   = var.meta
 }
 
-
 module "bucket" {
-  source = "github.com/terraform-aws-modules/terraform-aws-s3-bucket?ref=8a0b697adfbc673e6135c70246cff7f8052ad95a"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.3.0"
+
+
 
   bucket = var.bucket_name == "" ? module.meta.name : var.bucket_name
   acl    = var.acl
@@ -18,21 +20,20 @@ module "bucket" {
   attach_policy            = var.attach_policy
   website                  = var.website
   cors_rule                = jsonencode(var.cors_rule)
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowPublicRead",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${module.meta.name}/*"
-    }
-  ]
+  policy                   = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPublicRead",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${module.meta.name}/*"
+        }
+    ]
 }
 EOF
 }
