9beef3a8b7fcd63296afa959cf70f00c367744ce
dejan <dejan.pelemis@cyberlab.rs>
2024-07-19 14:27:14 +0200

    chore: Added meta modules

--- kms/main.tf
+++ kms/main.tf
@@ -1,3 +1,8 @@
+module "meta" {
+  source = "../meta"
+  meta   = var.meta
+}
+
 module "kms" {
   source  = "github.com/terraform-aws-modules/terraform-aws-kms?ref=fe1beca2118c0cb528526e022a53381535bb93cd"
 }
