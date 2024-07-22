e42d86784699ac03995384f3dc41bdef2fcaf03d
dejan <dejan.pelemis@cyberlab.rs>
2024-07-22 12:48:38 +0200

    chore: Adds variables for kms

--- kms/main.tf
+++ kms/main.tf
@@ -4,10 +4,18 @@ module "meta" {
 }
 
 
-
-module "kmss" {
+module "kms" {
   source  = "git::https://github.com/terraform-aws-modules/terraform-aws-kms?ref=fe1beca2118c0cb528526e022a53381535bb93cd"
+  
+  bypass_policy_lockout_safety_check = var.bypass_policy_lockout_safety_check
+  customer_master_key_spec           = var.customer_master_key_spec
+  custom_key_store_id                = var.custom_key_store_id
+  deletion_window_in_days            = var.deletion_window_in_days
+  description                        = var.description
+  enable_key_rotation                = var.enable_key_rotation
+  is_enabled                         = var.is_enabled
+  key_usage                          = var.key_usage
+  multi_region                       = var.multi_region
+  policy                             = coalesce(var.policy, data.aws_iam_policy_document.this[0].json)
+  rotation_period_in_days            = var.rotation_period_in_days
 }
-
-
-
