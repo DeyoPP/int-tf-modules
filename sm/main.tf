779b6362032c3f74cc2add95eae9ec28a89e82ac
dejan <dejan.pelemis@cyberlab.rs>
2024-07-31 13:52:20 +0200

    feat: Fixed vpc module

--- sm/main.tf
+++ sm/main.tf
@@ -1,6 +1,8 @@
 resource "aws_secretsmanager_secret" "example" {
   #chekcov:skip=CKV2_AWS_57
   #chekcov:skip=CKV_AWS_149
+  #checkov:skip=CKV_TF_1: "No need to use commit hash, easier to track with version"
+  #chekcov:skip=CKV2_AWS_64
   for_each = toset(var.secrets_names)
   name     = each.key
 }
