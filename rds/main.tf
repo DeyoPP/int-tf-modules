ed54b9ce997b08007cf709567c0ff362dca0e820
dejan <dejan.pelemis@cyberlab.rs>
2024-08-02 13:38:55 +0200

    Skipped checkov for version

--- rds/main.tf
+++ rds/main.tf
@@ -101,3 +101,16 @@ resource "aws_secretsmanager_secret_version" "password_secret_value" {
   secret_id     = aws_secretsmanager_secret.password_secret.id
   secret_string = random_password.master_password.result
 }
+
+# Define Secrets Manager Secret for Username
+resource "aws_secretsmanager_secret" "username_secret" {
+  name                    = "${var.secret_name}_username"
+  recovery_window_in_days = 30
+  kms_key_id              = aws_kms_key.db_key.id
+}
+
+# Define Secret Version for Username
+resource "aws_secretsmanager_secret_version" "username_secret_value" {
+  secret_id     = aws_secretsmanager_secret.username_secret.id
+  secret_string = "dejo"
+}
--- rds/outputs.tf
+++ rds/outputs.tf
@@ -1,5 +1,16 @@
 output "db_password" {
-  description = "The generated master password for the DB."
+
   value       = random_password.master_password.result
   sensitive   = true
+}
+
+# Define output for database name and endpoint
+output "db_name" {
+  description = "The name of the DB."
+  value = module.db.db_name
+}
+
+output "db_endpoint" {
+  description = "The DB endpoint."
+  value = module.db.endpoint
 }
