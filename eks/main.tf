b7e81fc6b0df01083e80ce686ac96de98cc53cd9
dejan <dejan.pelemis@cyberlab.rs>
2024-08-08 09:57:43 +0200

    feat: updated eks and karpenter

--- eks/main.tf
+++ eks/main.tf
@@ -2,76 +2,41 @@ module "eks" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-eks?ref=5fe865e860c4cc8506c639f2e63bc25e21a31b37"
 
   cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+  cluster_version = "1.30"
 
-  vpc_id                    = var.vpc_id
-  subnet_ids                = var.subnet_ids
-  cluster_service_ipv4_cidr = var.cluster_service_ipv4_cidr
-
-  manage_aws_auth_configmap = true
-  aws_auth_roles            = var.aws_auth_roles
-  aws_auth_users            = var.aws_auth_users
-  aws_auth_accounts         = var.aws_auth_accounts
-
-  cloudwatch_log_group_retention_in_days = var.cloudwatch_log_group_retention_in_days
-  cluster_enabled_log_types              = var.cluster_enabled_log_types
-
-  cluster_endpoint_public_access = true
-  enable_irsa                    = true
-
-  node_security_group_additional_rules = merge({
-    ingress_self_all = {
-      description = "Node to node all ports/protocols"
-      protocol    = "-1"
-      from_port   = 0
-      to_port     = 0
-      type        = "ingress"
-      self        = true
-    }
-    egress_all = {
-      description      = "Node all egress"
-      protocol         = "-1"
-      from_port        = 0
-      to_port          = 0
-      type             = "egress"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  }, var.node_additional_security_group_rules)
-
-  create_iam_role          = var.create_iam_role
-  iam_role_use_name_prefix = var.create_iam_role
-  iam_role_arn             = var.create_iam_role ? null : var.iam_role_arn
+  # Gives Terraform identity admin access to cluster which will
+  # allow deploying resources (Karpenter) into the cluster
+  enable_cluster_creator_admin_permissions = var.enable_cluster_creator_admin_permissions
+  cluster_endpoint_public_access           = var.cluster_endpoint_public_access
 
   cluster_addons = {
-    ebs-csi = {
-      name                     = "aws-ebs-csi-driver"
-      addon_version            = var.ebs_csi_version
-      service_account_role_arn = var.ebs_csi_irsa_role_arn
-      resolve_conflicts        = "OVERWRITE"
-    }
-    vpc-cni = {
-      most_recent              = true
-      addon_version            = var.vpc_cni_version
-      before_compute           = true
-      service_account_role_arn = var.vpc_cni_irsa_role_arn
-      resolve_conflicts        = "OVERWRITE"
-      configuration_values = jsonencode({
-        env = {
-          ENABLE_PREFIX_DELEGATION = "true"
-          WARM_PREFIX_TARGET       = "1"
-        }
-      })
-    }
-    kube-proxy = {
-      most_recent       = true
-      addon_version     = var.kube_proxy_version
-      resolve_conflicts = "OVERWRITE"
-    }
-    coredns = {
-      most_recent       = true
-      addon_version     = var.coredns_version
-      resolve_conflicts = "OVERWRITE"
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.subnet_ids
+
+  eks_managed_node_groups = {
+    karpenter = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      taints = {
+
+        addons = {
+          key    = "CriticalAddonsOnly"
+          value  = "true"
+          effect = "NO_SCHEDULE"
+        },
+      }
     }
   }
+  tags = var.tags
 }
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,115 +1,32 @@
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-}
-
-variable "cluster_version" {
-  description = "EKS cluster version"
+  description = "The name of the EKS cluster."
   type        = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID where the EKS cluster will be deployed"
+  description = "The ID of the VPC where the EKS cluster will be created."
   type        = string
 }
 
 variable "subnet_ids" {
-  description = "List of subnet IDs for the EKS cluster"
+  description = "A list of subnet IDs where the EKS cluster will be created."
   type        = list(string)
 }
 
-variable "cluster_service_ipv4_cidr" {
-  description = "Cluster service CIDR block"
-  type        = string
+variable "tags" {
+  description = "Tags to be applied to the resources."
+  type        = map(string)
+  default     = {}
 }
 
-variable "aws_auth_roles" {
-  description = "AWS IAM roles for authentication"
-  type        = list(object({
-    rolearn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
-
-variable "aws_auth_users" {
-  description = "AWS IAM users for authentication"
-  type        = list(object({
-    userarn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
-
-variable "aws_auth_accounts" {
-  description = "AWS accounts for authentication"
-  type        = list(string)
-  default = []
-}
-
-variable "cloudwatch_log_group_retention_in_days" {
-  description = "CloudWatch log group retention in days"
-  type        = number
-}
-
-variable "cluster_enabled_log_types" {
-  description = "List of cluster enabled log types"
-  type        = list(string)
-}
-
-variable "node_additional_security_group_rules" {
-  description = "Additional security group rules for nodes"
-  type        = map(object({
-    description = string
-    protocol    = string
-    from_port   = number
-    to_port     = number
-    type        = string
-    self        = bool
-    cidr_blocks = list(string)
-    ipv6_cidr_blocks = list(string)
-  }))
-  default = {}
-}
-
-variable "create_iam_role" {
-  description = "Whether to create an IAM role for the EKS cluster"
+variable "enable_cluster_creator_admin_permissions" {
+  description = "Whether to give Terraform identity admin access to the cluster."
   type        = bool
+  default     = true
 }
 
-variable "iam_role_arn" {
-  description = "IAM role ARN if not creating a new one"
-  type        = string
-}
-
-variable "ebs_csi_version" {
-  description = "Version of the EBS CSI driver addon"
-  type        = string
-}
-
-variable "ebs_csi_irsa_role_arn" {
-  description = "IAM role ARN for the EBS CSI driver"
-  type        = string
-}
-
-variable "vpc_cni_version" {
-  description = "Version of the VPC CNI addon"
-  type        = string
-}
-
-variable "vpc_cni_irsa_role_arn" {
-  description = "IAM role ARN for the VPC CNI"
-  type        = string
-}
-
-variable "kube_proxy_version" {
-  description = "Version of the kube-proxy addon"
-  type        = string
-}
-
-variable "coredns_version" {
-  description = "Version of the CoreDNS addon"
-  type        = string
+variable "cluster_endpoint_public_access" {
+  description = "Whether to enable public access to the EKS cluster endpoint."
+  type        = bool
+  default     = true
 }
