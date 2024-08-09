2df70f3c0f2962ec57992dfeb20e0e2c187a9193
Dejan <143999299+DeyoPP@users.noreply.github.com>
2024-08-09 16:03:02 +0200

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -24,9 +24,9 @@ module "eks" {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.medium"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 1
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
 
       taints = {
 
