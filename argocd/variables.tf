98c5aec36af4e0eede42748a3711d1200d6ab871
dejan <dejan.pelemis@cyberlab.rs>
2024-08-21 14:29:28 +0200

    Used terraform module for argocd

--- argocd/variables.tf
+++ argocd/variables.tf
@@ -1,17 +1,47 @@
+variable "release_name" {
+  type        = string
+  description = "Helm release name"
+  default     = "argocd"
+}
 variable "namespace" {
-  description = "Kubernetes namespace for ArgoCD"
+  description = "Namespace to install ArgoCD chart into"
   type        = string
   default     = "argocd"
 }
 
-variable "argo_cd_chart_version" {
-  description = "Version of the ArgoCD Helm chart"
+variable "argocd_chart_version" {
+  description = "Version of ArgoCD chart to install"
   type        = string
-  default     = "5.25.2"
+  default     = "5.27.1" # See https://artifacthub.io/packages/helm/argo/argo-cd for latest version(s)
 }
 
-variable "repository_url" {
-  description = "URL of the Helm repository for ArgoCD"
+# Helm chart deployment can sometimes take longer than the default 5 minutes
+variable "timeout_seconds" {
+  type        = number
+  description = "Helm chart deployment can sometimes take longer than the default 5 minutes. Set a custom timeout here."
+  default     = 800 # 10 minutes
+}
+
+variable "admin_password" {
+  description = "Default Admin Password"
   type        = string
-  default     = "https://argoproj.github.io/argo-helm"
+  default     = ""
+}
+
+variable "values_file" {
+  description = "The name of the ArgoCD helm chart values file to use"
+  type        = string
+  default     = "values.yaml"
+}
+
+variable "enable_dex" {
+  type        = bool
+  description = "Enabled the dex server?"
+  default     = true
+}
+
+variable "insecure" {
+  type        = bool
+  description = "Disable TLS on the ArogCD API Server?"
+  default     = false
 }
