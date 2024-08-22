edaa102b1e3609fc8ebb2b7cc716f5204a59e11e
dejan <dejan.pelemis@cyberlab.rs>
2024-08-22 14:05:38 +0200

    feat: New ext secrets module

--- external-secrets/service-account.tf
+++ external-secrets/service-account.tf
@@ -3,13 +3,13 @@ module "external_secret_service_account" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "5.30.0"
 
-  role_name                      = "${module.meta.name}-external-secret-role"
+  role_name                      = "${var.service_account_name}-external-secret-role"
   attach_external_secrets_policy = true
 
   oidc_providers = {
     ex = {
       provider_arn               = var.eks_oidc_provider_arn
-      namespace_service_accounts = ["kube-system:${module.meta.name}"]
+      namespace_service_accounts = ["kube-system:${var.service_account_name}"]
     }
   }
 }
