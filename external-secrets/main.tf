47fcb5602e4afff3ed0eb9f77916ebafa56c28dd
dejan <dejan.pelemis@cyberlab.rs>
2024-08-22 11:28:23 +0200

    Use yaml files for manifests

--- external-secrets/main.tf
+++ external-secrets/main.tf
@@ -29,83 +29,10 @@ resource "kubernetes_service_account" "external_secrets_sa" {
   }
 }
 
-# Kubernetes manifest for SecretStore
 resource "kubernetes_manifest" "secret_store" {
-  manifest = {
-    apiVersion = "external-secrets.io/v1beta1"
-    kind       = "SecretStore"
-    metadata = {
-      name      = var.secret_store_name
-      namespace = var.namespace
-    }
-    spec = {
-      provider = {
-        aws = {
-          service = "SecretsManager"
-          region  = var.aws_region
-          auth = {
-            serviceAccountRef = {
-              name = kubernetes_service_account.external_secrets_sa.metadata[0].name
-            }
-          }
-        }
-      }
-    }
-  }
+  manifest = var.secret_store_manifest
 }
 
-# Kubernetes manifest for ExternalSecret
 resource "kubernetes_manifest" "external_secret" {
-  manifest = {
-    apiVersion = "external-secrets.io/v1beta1"
-    kind       = "ExternalSecret"
-    metadata = {
-      name      = var.external_secret_name
-      namespace = var.namespace
-    }
-    spec = {
-      refreshInterval = var.refresh_interval
-      secretStoreRef = {
-        name = var.secret_store_name
-        kind = "SecretStore"
-      }
-      data = [
-        {
-          secretKey = "POSTGRES_USER"
-          remoteRef = {
-            key      = var.remote_secret_key
-            property = var.postgres_user_property
-          }
-        },
-        {
-          secretKey = "POSTGRES_PASSWORD"
-          remoteRef = {
-            key      = var.remote_secret_key
-            property = var.postgres_password_property
-          }
-        },
-        {
-          secretKey = "POSTGRES_DB"
-          remoteRef = {
-            key      = var.remote_secret_key
-            property = var.postgres_db_property
-          }
-        },
-        {
-          secretKey = "POSTGRES_HOST"
-          remoteRef = {
-            key      = var.remote_secret_key
-            property = var.postgres_host_property
-          }
-        },
-        {
-          secretKey = "POSTGRES_PORT"
-          remoteRef = {
-            key      = var.remote_secret_key
-            property = var.postgres_port_property
-          }
-        }
-      ]
-    }
-  }
+  manifest = var.external_secret_manifest
 }
