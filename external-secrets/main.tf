ae960f468d88c134734017fc0ca817fcb82c5180
Dejan <143999299+DeyoPP@users.noreply.github.com>
2024-08-26 18:37:11 +0200

    Update main.tf

--- external-secrets/main.tf
+++ external-secrets/main.tf
@@ -46,8 +46,6 @@ resource "helm_release" "external_secrets_resources" {
 module "external_secret_service_account" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam//modules/iam-role-for-service-accounts-eks?ref=89fe17a6549728f1dc7e7a8f7b707486dfb45d89"
 
-  version = "5.30.0"
-
   role_name                      = "external-secrets-role"
   attach_external_secrets_policy = true
 
