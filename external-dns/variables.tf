69028d6a86a16e4cf9f009e988458b499dca78e1
dejan <dejan.pelemis@cyberlab.rs>
2024-08-26 13:21:33 +0200

    feat: Added external dns

---
+++ external-dns/variables.tf
@@ -0,0 +1,10 @@
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "region" {
+  description = "The AWS region where the EKS cluster is located"
+  type        = string
+}
