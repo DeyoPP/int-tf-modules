916613484341ee9c2db146d4aedc4fd194a32434
dejan <dejan.pelemis@cyberlab.rs>
2024-08-27 11:19:38 +0200

    fix: External secrets annotation update

--- external-dns/main.tf
+++ external-dns/main.tf
@@ -97,17 +97,121 @@ resource "helm_release" "external_dns" {
     value = "true"
   }
 
-  # Set the Service Account name
   set {
     name  = "serviceAccount.name"
     value = "external-dns"
   }
 
-  # Set annotations using a map
+GA# Create IAM role and policy for ExternalDNS
+resource "aws_iam_role" "external_dns" {
+  name = "${var.cluster_name}-external-dns"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        Federated = var.oidc_provider
+      }
+      Condition = {
+        StringEquals = {
+          "${var.oidc_provider}:sub" = "system:serviceaccount:${var.namespace}:external-dns"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "external_dns_policy" {
+  name   = "${var.cluster_name}-external-dns-policy"
+  role   = aws_iam_role.external_dns.name
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = [
+        "route53:ChangeResourceRecordSets",
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+}
+
+# Deploy the ExternalDNS Helm chart and let it create the Service Account
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  namespace  = var.namespace
+
   set {
-    name  = "serviceAccount.annotations"
-    value = jsonencode({
-      "eks.amazonaws.com/role-arn" = aws_iam_role.external_dns.arn
-    })
+    name  = "provider"
+    value = "aws"
   }
+
+  set {
+    name  = "aws.zoneType"
+    value = var.zone_type
+  }
+
+  set {
+    name  = "policy"
+    value = var.policy
+  }
+
+  set {
+    name  = "registry"
+    value = var.registry
+  }
+
+  set {
+    name  = "aws.region"
+    value = var.region
+  }
+
+  set {
+    name  = "txtOwnerId"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "domainFilters[0]"
+    value = var.domain_filter
+  }
+
+  set {
+    name  = "sources"
+    value = "{service,ingress}"
+  }
+
+  set {
+    name  = "txtPrefix"
+    value = var.txt_prefix
+  }
+
+  set {
+    name  = "aws.hostedZoneID"
+    value = var.hosted_zone_id
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+# it is highly recommended to enable webhook and certController according to: https://external-secrets.io/v0.8.0/api/components/
+  set {
+    name  = "webhook.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.external_secret_service_account.iam_role_arn
+  }
+
+}
+
 }
