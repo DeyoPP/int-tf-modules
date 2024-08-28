948b9f429eef10eb64ea369fd0399015b4744c2e
dejan <dejan.pelemis@cyberlab.rs>
2024-08-28 11:38:59 +0200

    feat: Finshed modules

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,6 +1,6 @@
 output "cluster_name" {
   description = "The name of the EKS cluster"
-  value       = module.eks.cluster_id
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
