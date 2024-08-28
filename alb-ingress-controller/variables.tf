46f188be527085c26bfbe38f398cff0b4c29a9ed
dejan <dejan.pelemis@cyberlab.rs>
2024-08-28 09:03:54 +0200

    feat: Added alb ingress

--- alb-ingress-controller/variables.tf
+++ alb-ingress-controller/variables.tf
@@ -13,3 +13,21 @@ variable "vpc_id" {
   description = "The VPC ID where the ALB Ingress Controller will be deployed"
   type        = string
 }
+
+variable "oidc_provider" {
+  description = "The OIDC provider ARN for the EKS cluster"
+  type = string
+  default   = ""
+}
+
+variable "oidc_url" {
+  description = "The OIDC provider URL for the EKS cluster"
+  type = string
+  default   = ""
+}
+
+variable "namespace" {
+  description = "The Kubernetes namespace where the external-dns will be deployed"
+  type        = string
+  default     = "kube-system"
+}
