663c3d5d76d9f52a107a2a614a7dbbfde749f603
dejan <dejan.pelemis@cyberlab.rs>
2024-08-30 09:47:14 +0200

    feat: New modules

--- external-secrets/main.tf
+++ external-secrets/main.tf
@@ -75,31 +75,6 @@ resource "helm_release" "external_secrets" {
 }
 
 
-resource "helm_release" "external_secrets_resources" {
-  name      = "${var.namespace}-resources"
-  namespace = var.namespace
-
-  chart = "${path.module}/charts/external-secrets-resources"
-
-  set {
-    name  = "serviceAccount.name"
-    value = "external-secrets"
-  }
-
-  set {
-    name  = "serviceAccount.namespace"
-    value = var.namespace
-  }
-
-  set {
-    name  = "region"
-    value = "eu-central-1"
-  }
-
-  depends_on = [
-    helm_release.external_secrets
-  ]
-}
 
 # resource "kubernetes_manifest" "external_secret" {
 #   manifest = {
